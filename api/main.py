8b165a88f26920c3671d937a5f842ad2cb363cde
Sandugash <sshaikhina@gmail.com>
2026-06-10 20:52:21 +0300

    changed the url

--- api/main.py
+++ api/main.py
@@ -18,7 +18,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000"],
+    allow_origins=["https://domprice-kz.vercel.app", "http://127.0.0.1:3000"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
